8ec7bdcf38ed49066734c612031d8935eed62e8f
petreastefann <petreastefann@gmail.com>
2023-04-24 19:07:26 +0300

    added: requirements, updated appsettings.json and added migration

--- DataLayer/Entities/Grade.cs
+++ DataLayer/Entities/Grade.cs
@@ -2,11 +2,21 @@
 
 namespace DataLayer.Entities;
 
-public class Grade : BaseEntity
-{
-    public double Value { get; set; }
-    public CourseType Course { get; set; }
+public class Grade : BaseEntity {
+	public double Value {
+		get; set;
+	}
+	public CourseType Course {
+		get; set;
+	}
+	public DateTime DateCreated {
+		get; set;
+	}
 
-    public int StudentId { get; set; }
-    public Student Student { get; set; }
+	public int StudentId {
+		get; set;
+	}
+	public Student Student {
+		get; set;
+	}
 }
